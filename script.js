33562d770181f265aaea8b431a172726a2b8de06
ggnika1337 <nikushanika568@gmail.com>
2026-01-07 00:36:53 +0400

    update
    added lot of things

--- script.js
+++ script.js
@@ -1,19 +1,44 @@
 // THEME CHANGER BUTTON ANIMATION
 
 const themeBtn = document.querySelector(".theme-btn");
-const themeDot = document.querySelector(".theme-dot");
+const themeBtnOverlay = document.querySelector(".theme-btn-overlay");
 let isRight = false;
 
 themeBtn.addEventListener("click", () => {
   if (themeBtn.style.justifyContent === "flex-end") {
     themeBtn.style.justifyContent = "flex-start";
+    themeBtnOverlay.style.justifyContent = "flex-start";
   } else {
+    themeBtn.style.justifyContent = "flex-end";
+    themeBtnOverlay.style.justifyContent = "flex-end";
+  }
+  isRight = !isRight;
+});
+
+themeBtnOverlay.addEventListener("click", () => {
+  if (themeBtnOverlay.style.justifyContent === "flex-end") {
+    themeBtn.style.justifyContent = "flex-start";
+    themeBtnOverlay.style.justifyContent = "flex-start";
+  } else {
+    themeBtnOverlay.style.justifyContent = "flex-end";
     themeBtn.style.justifyContent = "flex-end";
   }
   isRight = !isRight;
 });
 
-console.log(isRight);
+// CURRENT TIME LOGIC
+
+let time = document.getElementById("time");
+let timeOverlay = document.getElementById("timeOverlay");
+
+const currentDate = new Date();
+
+const weekday = currentDate.toLocaleString("en-US", { weekday: "short" });
+const day = currentDate.getDate();
+const month = currentDate.toLocaleString("en-US", { month: `numeric` });
+
+time.innerHTML = `${weekday.toUpperCase()} ${day}.${month}`;
+timeOverlay.innerHTML = `${weekday.toUpperCase()} ${day}.${month}`;
 
 // HAMBURGER
 
@@ -36,5 +61,30 @@ burgerMenu.addEventListener("click", function () {
   } else {
     burgerX.style.display = "block";
     burger.style.display = "none";
+  }
+});
+
+// GET VIDEO LENGTH LOGIC
+
+let videoLength = document.getElementById("videoLength");
+let video = document.querySelector(".videoFirst");
+
+video.addEventListener("loadedmetadata", () => {
+  let mins = Math.floor(video.duration / 60);
+  let secs = Math.floor(video.duration % 60);
+  videoLength.innerHTML = `${mins}:${secs.toString().padStart(2, "0")}`;
+});
+
+// VIDEO PLAY / PAUSE LOGIC
+
+let playBtn = document.querySelector(".video-btn");
+
+playBtn.addEventListener("click", () => {
+  if (video.paused) {
+    video.play();
+    playBtn.style.opacity = 0.1;
+  } else {
+    video.pause();
+        playBtn.style.opacity = 1;
   }
 });
